d37a80367c44ae4dc4ee33cd3e1bd1710a344fcb
Lovenish Gaur <lovenishgaur9@gmail.com>
2020-06-04 08:15:09 +0530

    Add files via upload

---
+++ Python_Programs/Hello.py
@@ -0,0 +1,11 @@
+# This Programm says Hello and Ask for my Name
+
+print('hello World!..')
+print('Whats Your name')
+myname = input()
+print('It is good to meet you, '+ myname)
+print('The length of your name is')
+print(len(myname))
+print('Whatis your age')
+Age = input()
+print('You`ll be ' + str(int(Age) + 1 ) + ' In a year ')      
